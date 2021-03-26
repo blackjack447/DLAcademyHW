25f4a16c045544b41cf881089aa8f8f99e0ec94c
blackjack447 <lovygin@rambler.ru>
2021-03-26 20:21:59 +0400

    исправил 10 дз

--- Lesson_10/dz.py
+++ Lesson_10/dz.py
@@ -4,7 +4,10 @@
 # название продукта, стоимость, количество. Разработать класс FruitProduct
 # на базе класс Product, фрукт характеризуется параметрами:
 # страна изготовителя, срок годности.
-class Products:
+from datetime import date
+
+
+class Product:
     def __init__(self, id_item, name, cost, quantity):
         self.id = id_item
         self.name = name
@@ -12,8 +15,9 @@ class Products:
         self.quantity = quantity
 
 
-class FruitProduct(Products):
-    def __init__(self, id_item, name, cost, quantity, country, expiration_date):
+class FruitProduct(Product):
+    def __init__(self, id_item, name, cost, quantity, country,
+                 expiration_date):
         self.country = country
         self.expiration_date = expiration_date
         super().__init__(id_item, name, cost, quantity)
@@ -24,44 +28,105 @@ class Shop:
         self.name = name
         self.adress = adress
         self.product_lst = product_lst
-        
+
     @property
-    def get_shop_name(self):
+    def shop_name(self):
         return self.name
 
     @property
-    def get_shop_adress(self):
+    def shop_adress(self):
         return self.adress
+
+    def show_menu(self):
+        print(
+            f'Вас приветствует магазин {self.shop_name}, мы располагаемся по адресу: {self.shop_adress}, что желаете ?'
+        )
+        print('1. Купить', '2. Добавить товар', '3. Удалить товары, чей срок годности истек', '4. Выйти', sep='\n')
+
+    def show_products(self):
+        for i in self.product_lst:
+            print(
+                f'В продаже есть {i.name} цене {i.cost} за кг. в кол-ве {i.quantity} кг.'
+            )
+
+    def add_product(self, product):
+        in_shop = False
+        for i in self.product_lst:
+            if i.id == product.id:
+                in_shop = True
+                print(
+                    f'Товар {product.name} с таким id {product.id} уже есть у нас в магазине!'
+                )
+        if not in_shop:
+            self.product_lst.append(product)
+            print(f'Товар {product.name} -добавлен!')
+
+    def del_product(self, product):
+        try:
+            self.product_lst.remove(product)
+            print(f'Товар {product.name} - удален!')
+        except ValueError:
+            print(f'Такого товара {product.name} у нас в магазине нету. Удаление - невозможно')
+
+    def check_date(self, product_date, today_date):
+        p_year, p_month, p_day = map(int, product_date.split('.'))
+        t_year, t_month, t_day = map(int, today_date.split('-'))
+        if p_year < t_year:
+            return False
+        elif p_year == t_year and p_month < t_month:
+            return False
+        elif p_year == t_year and p_month == t_month and p_day < t_day:
+            return False
+        else:
+            return True
+
+    def del_expired_products(self, today_date):
+        print('Проверка срока годности товара...')
+        for i in self.product_lst:
+            if not self.check_date(i.expiration_date, today_date):
+                self.del_product(i)
+
+    def buy_product(self):
+        h = str(input('Что хотите купить? - '))
+        for i in self.product_lst:
+            if i.name == h:
+                i.quantity -= 1
+                print(f'Спасибо за покупку, вот Ваши {i.name}')
+                for i in shop.product_lst:
+                    print(
+                        f'В продаже остались {i.name} цене {i.cost} за кг. в кол-ве {i.quantity} кг.'
+                    )
 
 
 if __name__ == '__main__':
-    fruits = [FruitProduct('11970012', 'Апельсины', 350, 30, 'Морокко', 30),
-              FruitProduct('10151078', 'Бананы', 150, 98, 'Бразилия', 25),
-              FruitProduct('14872056', 'Персики', 450, 75, 'Абхазия', 60)]
+    fruits = [
+        FruitProduct('11970012', 'Апельсины', 350, 30, 'Морокко', '2007.12.30'),
+        FruitProduct('10151078', 'Бананы', 150, 98, 'Бразилия', '2022.02.25'),
+        FruitProduct('14872056', 'Персики', 450, 75, 'Абхазия', '2021.07.19')
+    ]
 
     shop = Shop('"У Ашотика"', 'ул.Шашлычная,д.5', fruits)
 
-    magazin_name = shop.get_shop_name
-    magazin_adress = shop.get_shop_adress
-    print(f'Вас приветствует магазин {magazin_name}, мы располагаемся по адресу: {magazin_adress}')
-
-    for i in shop.product_lst:
-        print(f'В продаже есть {i.name} цене {i.cost} за кг. в кол-ве {i.quantity} кг.')
-
-    h = str(input('Что хотите купить? - '))
-    for i in shop.product_lst:
-        if i.name == h:
-            i.quantity -= 1
-            print(f'Спасибо за покупку, вот Ваши {i.name}')
-            for i in shop.product_lst:
-                print(f'В продаже остались {i.name} цене {i.cost} за кг. в кол-ве {i.quantity} кг.')
-
-    a = FruitProduct('11082054', 'Финики', 100, 15, 'Египет', 100)
-    b = fruits.append(a)
-    c = FruitProduct('11111111', 'Хурма', 35, 10, 'Подстепки', 8)
-    d = fruits.append(c)
-
-    k = 10
-    for i in shop.product_lst:
-        if i.expiration_date < k:
-            print(f'Срок хранения товара {i.name} подходит к концу')
+    finish = False
+    while not finish:
+        shop.show_menu()
+        action = int(input())
+        if action == 1:
+            shop.show_products()
+            shop.buy_product()
+        elif action == 2:
+            id = str(input('Введите id товара:'))
+            name = str(input('Введите название товара:'))
+            cost = int(input('Введите стоимость:'))
+            value = int(input('Введите кол-во:'))
+            country = str(input('Введите страну происхождения:'))
+            date = str(input('Введите срок годности (год, месяц,день):'))
+            g = FruitProduct(id, name, cost, value, country, date)
+            shop.add_product(g)
+        elif action == 3:
+            # pr_name = input('Введите название продукта - ')
+            data = date.today()
+            split_date = date.strftime(data, '%Y-%m-%d')
+            shop.del_expired_products(split_date)
+        elif action == 4:
+            finish = True
